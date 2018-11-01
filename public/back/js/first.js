b7103aabe2e9414d038c5c1b835cced62bf9f8f0
zhang-ju <343659485@qq.com>
2018-11-01 19:05:15 +0800

    一级分类添加动能完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -12,9 +12,9 @@ $(function(){
       },
       dataType:'json',
       success:function(info){
-        console.log(info);
+        // console.log(info);
         var str=template('firstTmp',info);
-        console.log(str);
+        // console.log(str);
         
         $('tbody').html(str);
         $('#fstpaginator').bootstrapPaginator({
@@ -43,11 +43,47 @@ $(function(){
 
   }
 
+// 点击添加分类按钮，弹出模态框；
+$('#addBtn').on('click',function(){
+  $('#addModal').modal('show');
+})
 
+// 表单校验；
+$('#form').bootstrapValidator({
+  feedbackIcons:{
+    valid:'glyphicon glyphicon-ok',//校验成功
+    invalid:'glyphicon glyphicon-remove',//校验失败
+    validating:'glyphicon glyphicon-refresh'//校验中
+  },
+  fields:{
+    categoryName:{
+      validators:{
+        notEmpty:{
+          message:'请输入一级分类'
+        }
+      }
+    }
+  }
+});
 
-
-
-
+// 给表单注册校验成功事件
+$('#form').on('success.form.bv',function(e){
+  e.preventDefault();
+  $.ajax({
+    type:'post',
+    url:'/category/addTopCategory',
+    data:$('#form').serialize(),
+    dataType:'json',
+    success:function(info){
+      // console.log(info);
+      $('#addModal').modal('hide');
+      currentPage=1;
+      render();
+      // 将表单中的内容和校验状态都重置
+     $('#form').data('bootstrapValidator').resetForm(true);
+    }
+  })
+})
 
 
 
