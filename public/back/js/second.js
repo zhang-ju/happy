bf941258999dbf11ac11a8e0214b39569594eb9d
zhang-ju <343659485@qq.com>
2018-11-01 20:05:51 +0800

    数据及分页的渲染完成

---
+++ public/back/js/second.js
@@ -0,0 +1,58 @@
+$(function(){
+  var currentPage=1;
+  var pageSize=5;
+  render();//通过ajax渲染数据到表格中；
+  function render(){
+    $.ajax({
+      type:'get',
+      url:'/category/querySecondCategoryPaging',
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      dataType:'json',
+      success:function(info){
+        console.log(info);
+        var str=template('secTmp',info);
+        $('tbody').html(str);
+        $('#secpaginator').bootstrapPaginator({
+          bootstrapMajorVersion:3,
+          totalPages:Math.ceil(info.total/info.size),
+          currentPage:info.page,
+          onPageClicked:function(a,b,c,page){
+            currentPage=page;
+            render();
+          }
+        })
+      }
+    })
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
